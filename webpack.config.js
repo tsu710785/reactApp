56de69f389e007891d8ced2b5af22bd9079ce3fe
azure820529 <azure820529@gmail.com>
2016-08-01 22:31:42 +0800

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
 var config = {
    entry: './main.js',
-	
+
    output: {
       path:'./',
       filename: 'index.js',
    },
-	
+
    devServer: {
       inline: true,
       port: 3000
    },
-	
+
    module: {
       loaders: [
          {
             test: /\.jsx?$/,
             exclude: /node_modules/,
             loader: 'babel',
-				
+
             query: {
                presets: ['es2015', 'react']
             }
