007b9659662f63e7c695ead45240e1b5d206957b
azure820529 <azure820529@gmail.com>
2016-08-01 22:31:12 +0800

    make code work and run simple UI

--- App.jsx
+++ App.jsx
@@ -1,67 +1,75 @@
 import React from 'react';
 
 
-class ProductList extends React.Component {
-	constructor(props){
-   		super(props);
-   		console.log(props.datas);
-
-	}
-  	render() {
-      	return (
-      		<h5>
-      			{this.props.datas}
-      		</h5>
-      	);
-   	}
-}
+// class ProductList extends React.Component {
+// 	constructor(props){
+//    		super(props);
+//    		console.log(props.datas);
+//
+// 	}
+//   	render() {
+//       	return (
+//       		<h5>
+//       			{this.props.datas}
+//       		</h5>
+//       	);
+//    	}
+// }
 
 class SearchBar extends React.Component {
-   
+
    render() {
       return (
-         <div>
-         	<input type="text"/>
-         </div>
+       <div>
+       	<input type="text"/>
+       </div>
       );
    }
 }
 
 class Product extends React.Component {
-   	
    	constructor(props){
    		super(props);
-
-   		// this.row = {
-   		// 	"a":"a"
-   		// 	// var rows=[];
-   		// 	// props.products.forEach(val=>this.rows.push(val));
-   		// 	// // console.log(row);
-   		// 	// return rows;
-   		// };
-   		// var row=[];
-   		console.log(props.products);
-   		
-   		
    	}
 
 	render() {
+		var products = this.props.products;
 		return (
-        	<ProductList datas={this.props.products.price} key={this.props.products.category} />
-
-      	);
+      	<div>
+					{products.map(function(p) {
+						return (
+							<div>
+								<h3>{p.category}</h3>
+								<h3>{p.price}</h3>
+							</div>
+						);
+					})}
+      	</div>
+    	);
    	}
 }
 
-// Product.defaultProps = { row: [] };
 
 class ProductTable extends React.Component {
-   
+  constructor(props){
+
+		super(props);
+
+		this.state = {
+			text: "product list",
+			products: [
+				{category:"A",price:"99.99"},
+				{category:"B",price:"10"}
+			]
+		};
+
+	}
+
    render() {
       return (
          <div>
             <SearchBar/>
-            <Product products={this.props.products}/>
+            <Product products={this.state.products}/>
          </div>
       );
    }
--- main.js
+++ main.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ProductTable from './App.jsx';
 
-var products = [
-{category:"A",price:"99.99"},
-{category:"B",price:"10"}
-];
 
 
-ReactDOM.render(<ProductTable products={products}/>, document.getElementById('ProductTable'));
 
+ReactDOM.render(<ProductTable/>, document.getElementById('ProductTable'));
